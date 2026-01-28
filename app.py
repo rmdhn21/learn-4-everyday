7dff7d666970fa6f6bcf794dc44b26ad74681345
rmdhn21 <rma21dhan@gmail.com>
2026-01-28 23:16:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if not api_key:
     st.stop()
 
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 
 tab1, tab2 = st.tabs(["📝 Tanya Teks", "📸 Tanya Gambar"])
 
